8c5be8ea305495446683f43370bf33e984e02a72
lastuck <wolf.hugo7@gmail.com>
2021-05-03 15:55:49 +0200

    init kebabiste

---
+++ KebabisteRoyale/Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Ingredient
+{
+    Sheep,
+    Chicken,
+    Steak,
+    Salad,
+    Onions,
+    Tomatoes,
+    Samurai,
+    White,
+    BBQ,
+    Ketchup,
+    Mayo
+}
+
+public class GameController : MonoBehaviour
+{
+    public static Dictionary<Ingredient, int> ingredientPrices;
+    public static Kebabiste kebabiste1;
+    public static Kebabiste kebabiste2;
+    
+    private void Start()
+    {
+        InitPrices();
+        kebabiste1 = new Kebabiste();
+        kebabiste2 = new Kebabiste();
+    }
+
+    private void InitPrices()
+    {
+        ingredientPrices.Add(Ingredient.Sheep, 100);
+        ingredientPrices.Add(Ingredient.Chicken, 100);
+        ingredientPrices.Add(Ingredient.Steak, 100);
+        ingredientPrices.Add(Ingredient.Salad, 100);
+        ingredientPrices.Add(Ingredient.Onions, 100);
+        ingredientPrices.Add(Ingredient.Tomatoes, 100);
+        ingredientPrices.Add(Ingredient.Samurai, 100);
+        ingredientPrices.Add(Ingredient.White, 100);
+        ingredientPrices.Add(Ingredient.BBQ, 100);
+        ingredientPrices.Add(Ingredient.Ketchup, 100);
+        ingredientPrices.Add(Ingredient.Mayo, 100);
+    }
+}
---
+++ KebabisteRoyale/Assets/Scripts/Kebabiste.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kebabiste
+{
+    public enum Action
+    {
+        None,
+        OrderIngredient,
+        CreateDish,
+        FakeClient,
+        Corrupt
+    }
+    
+
+    private int money;
+    private int stress;
+    public const int MAX_STRESS = 100;
+    private Dictionary<Ingredient, int> ingredientAmounts;
+    
+    
+    public Kebabiste()
+    {
+        foreach (Ingredient ingredient in Enum.GetValues(typeof(Ingredient)))
+        {
+            ingredientAmounts.Add(ingredient, 0);
+        }
+
+        money = 1000;
+        stress = 0;
+    }
+
+    public virtual Action GetIntent()
+    {
+        return Action.None;
+    }
+}
